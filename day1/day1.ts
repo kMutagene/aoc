18845084c828a8e502002312f65b1bc8f2d11b14
Kevin Schneider <schneider.kev@outlook.de>
2021-12-02 10:04:44 +0100

    solve day1 part 1

--- day1/day1.ts
+++ day1/day1.ts
@@ -1,1 +1,17 @@
-console.log("soos")
+import * as fs from 'fs';
+
+const data = 
+    fs.readFileSync('C:/Users/schne/source/repos/kMutagene/aoc2021/day1/input.txt', 'utf8')
+        .split("\n")
+
+let count = 0
+
+data.map((val,index) => {
+    if (index > 0) {
+        if (parseInt(val) > parseInt(data[index - 1])) {
+            count++
+        }
+    }
+})
+
+console.log(count)
