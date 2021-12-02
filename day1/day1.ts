cdf782c34c863e325b981976cdc44be9b281af59
Kevin Schneider <schneider.kev@outlook.de>
2021-12-02 10:40:02 +0100

    complete day 1 part 2

--- day1/day1.ts
+++ day1/day1.ts
@@ -2,16 +2,41 @@ import * as fs from 'fs';
 
 const data = 
     fs.readFileSync('C:/Users/schne/source/repos/kMutagene/aoc2021/day1/input.txt', 'utf8')
-        .split("\n")
+    .split("\n")
+    .map (x => parseInt(x))
 
+// part1
 let count = 0
 
 data.map((val,index) => {
     if (index > 0) {
-        if (parseInt(val) > parseInt(data[index - 1])) {
+        if (val > data[index - 1]) {
             count++
         }
     }
 })
 
 console.log(count)
+
+// part2
+
+let windowSum =
+    data
+    .map((_,index) => {
+        return data.slice(index, index+3)
+    })
+    .filter(x => x.length === 3)
+    .map(x => {
+        return x.reduce((a,b) => a + b)
+    })
+
+let countWindowed = 0
+
+windowSum.map((val,index) => {
+    if (index > 0) {
+        if (val > windowSum[index - 1]) {
+            countWindowed ++
+        }
+    }
+})
+console.log(countWindowed)
