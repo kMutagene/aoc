dc6ddef5eda29f8f680d2662a434ba157be65d9b
Kevin Schneider <schneider.kev@outlook.de>
2021-12-03 11:14:33 +0100

    finish day2 part2

--- day2/day2.ts
+++ day2/day2.ts
@@ -21,6 +21,7 @@ const data =
 let depth = 0
 let horizontal = 0
 
+//part1
 data
 .map((action) => {
     switch (action.Direction) {
@@ -42,4 +43,35 @@ data
     }
 })
 
-console.log(`depth: ${depth} horizontal: ${horizontal} hash:${depth*horizontal}`)
+console.log(`[PART1] depth: ${depth} horizontal: ${horizontal} hash:${depth*horizontal}`)
+
+
+//part2
+depth = 0
+horizontal = 0
+let aim = 0
+
+data
+.map((action) => {
+    switch (action.Direction) {
+        case "forward":
+            horizontal += action.Speed
+            depth += (aim*action.Speed)
+            console.log(`horizontal: ${horizontal}`)
+            console.log(`depth: ${depth}`)
+            break
+        case "up":
+            aim -= action.Speed
+            console.log(`aim: ${aim}`)
+            break
+        case "down":
+            aim += action.Speed
+            console.log(`aim: ${aim}`)
+            break
+        default:
+            console.log(`lol? ${action.Direction}`)
+            break
+    }
+})
+
+console.log(`[PART2] depth: ${depth} horizontal: ${horizontal} hash:${depth*horizontal}`)
